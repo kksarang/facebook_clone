b71b4da2506b53f9264ed694eb064694d9ca3d56
SarangrVAASITS <97729855+SarangrVAASITS@users.noreply.github.com>
2022-10-17 07:56:00 +0530

    appbar added

--- lib/home.dart
+++ lib/home.dart
@@ -1,12 +1,48 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Text('sdsdfds'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          title: const Text(
+            'facebook',
+            style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold, color: Colors.blue),
+          ),
+          actions: [
+            Container(
+              margin: const EdgeInsets.all(3),
+              decoration: BoxDecoration(color: Colors.grey[100], shape: BoxShape.circle),
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.search,
+                  color: Colors.black,
+                  size: 25,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(5),
+              decoration: BoxDecoration(color: Colors.grey[100], shape: BoxShape.circle),
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.chat_bubble_outline,
+                  color: Colors.black,
+                  size: 25,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/appBarButton.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class AppBarTheme extends StatelessWidget {
+  const AppBarTheme({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
