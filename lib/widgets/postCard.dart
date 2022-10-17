065194b4a3344b6ba396f82108d1511dfda8245a
kksarang <sarangrajan17@gmail.com>
2022-10-17 19:18:57 +0530

    latest changes

--- lib/widgets/postCard.dart
+++ lib/widgets/postCard.dart
@@ -1,9 +1,9 @@
-import 'package:facebook_clone/widgets/blueTick.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../assets.dart';
 import 'avathar.dart';
+import 'blueTick.dart';
 
 class PostCard extends StatelessWidget {
   final String avathar;
@@ -16,45 +16,78 @@ class PostCard extends StatelessWidget {
     return Container(
       child: Column(
         children: [
-          ListTile(
-            leading: Avathar(
-              displayImageName: networkThree,
-              displayStatus: false,
-            ),
-            title: Row(
-              children: [
-                Text(
-                  name,
-                  style: const TextStyle(color: Colors.black),
-                ),
-                const SizedBox(
-                  width: 6,
-                ),
-                BlueTick()
-              ],
-            ),
-            subtitle: Row(
-              children: [
-                Text(publishAt == null ? "" : publishAt),
-                const SizedBox(
-                  width: 10,
-                ),
-                Icon(
-                  Icons.public,
-                  color: Colors.grey[700],
-                  size: 14,
-                )
-              ],
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.more_horiz,
-                color: Colors.grey[700],
-              ),
-            ),
+          postCardHeader(),
+          titleSelection(),
+          const SizedBox(
+            height: 4,
+          ),
+          imagePosting()
+        ],
+      ),
+    );
+  }
+
+  Widget imagePosting() {
+    return Container(
+      child: Image.network(
+        aappu,
+      ),
+    );
+  }
+
+  Widget titleSelection() {
+    return Container(
+      child: Row(
+        children: const [
+          SizedBox(
+            width: 20,
+          ),
+          Text(
+            "Happy Birthday",
+            style: TextStyle(color: Colors.black, fontSize: 16),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget postCardHeader() {
+    return ListTile(
+      leading: Avathar(
+        displayImageName: networkThree,
+        displayStatus: false,
+      ),
+      title: Row(
+        children: [
+          Text(
+            name,
+            style: const TextStyle(color: Colors.black),
+          ),
+          const SizedBox(
+            width: 6,
+          ),
+          BlueTick()
+        ],
+      ),
+      subtitle: Row(
+        children: [
+          Text(publishAt == null ? "" : publishAt),
+          const SizedBox(
+            width: 10,
+          ),
+          Icon(
+            Icons.public,
+            color: Colors.grey[700],
+            size: 14,
           )
         ],
+      ),
+      trailing: IconButton(
+        onPressed: () {},
+        icon: Icon(
+          Icons.more_horiz,
+          color: Colors.grey[700],
+        ),
       ),
     );
   }
